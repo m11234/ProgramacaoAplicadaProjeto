7fe858ca54d2cbdcb82329a86938b3d5df77ccd1
henrique <ribeirosantoshenrique2004@gmail.com>
2026-03-21 14:56:17 +0000

    Validação de email

--- src/controller/UtilizadorController.java
+++ src/controller/UtilizadorController.java
@@ -1,12 +1,21 @@
 package controller;
 
+
 import java.util.Scanner;
+import java.util.regex.Pattern; //importação para a validação do email
 
 import model.Utilizador;
 import model.dao.UtilizadoresDAO;
 public class UtilizadorController {
 
     private UtilizadoresDAO dao = new UtilizadoresDAO();
+
+
+    //email
+    private static final String EMAIL_REGEX = "^[^@\\s]+@[^@\\s]+\\.[^@\\s]{2,}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+
 
     public void registar(Scanner sc) {
 
@@ -21,8 +30,20 @@ public class UtilizadorController {
         System.out.print("Password: ");
         String password = sc.nextLine();
 
-        System.out.print("Email: ");
-        String email = sc.nextLine();
+        String email = "";
+        boolean emailValido = false;
+
+        //BASEADO NESTA IMPLENMENTAÇÃO: https://www.abstractapi.com/guides/api-functions/email-validation-in-java
+        while (!emailValido) {
+            System.out.print("Email: ");
+            email = sc.nextLine();
+
+            if (EMAIL_PATTERN.matcher(email).matches()) {
+                emailValido = true;
+            } else {
+                System.out.println("Erro: O email deve apresentar um formato válido e obrigatório ([designação] @ [entidade] . [domínio]).");
+            }
+        }
 
         String estado = "ativo";
 
