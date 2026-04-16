3a19cfa76aba67b2398a224e341d7ff7137a4ca5
m11234 <miguelafonsoplacido@gmail.com>
2026-04-17 00:15:52 +0100

    tempo desde que começou a correr o programa

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -4,9 +4,18 @@ import model.db.DBConnection;
 import view.Menu;
 
 import java.sql.SQLException;
-
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
 public class Main {
   static void main() throws SQLException {
+    /**
+     * Solução adaptada de: Stackify
+     * Fonte: <a href="https://stackify.com/heres-how-to-calculate-elapsed-time-in-java/">Here's How to Calculate Elapsed Time in Java</a>
+     * Acedido em: 17 de Abril de 2026.
+     */
+    Instant start = Instant.now();
+    LocalDateTime startDateAndTime = LocalDateTime.now();
 
     if (DBConnection.getconn() == null) {
       System.out.println("Erro na ligação à BD.");
@@ -15,5 +24,12 @@ public class Main {
 
     Menu menu = new Menu();
     menu.start();
+    // time passes
+    Instant end = Instant.now();
+    LocalDateTime endDateAndTime = LocalDateTime.now();
+    Duration timeElapsed = Duration.between(start, end);
+    System.out.println("Início do processo:" + startDateAndTime);
+    System.out.println("Tempo total: " + timeElapsed.toSeconds() + " segundos.");
+    System.out.println("Fim do processo:" + endDateAndTime);
   }
 }
