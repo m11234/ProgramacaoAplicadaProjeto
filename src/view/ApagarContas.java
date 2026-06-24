2964402db363150e56e6d2282105965d59f66089
Henriqueantos <wardwin@gmail.com>
2026-06-24 14:34:10 +0100

    ApagarContas

--- src/view/ApagarContas.java
+++ src/view/ApagarContas.java
@@ -8,10 +8,35 @@ import java.util.List;
 import java.awt.*;
 import java.sql.SQLException;
 
+
+/**
+ * Classe responsável por criar a interface gráfica para a eliminação de contas de utilizadores
+ * <p>
+ * Esta classe estende {@link JPanel} e disponibiliza ao administrador uma tabela que contém as contas
+ * elegíveis para eliminação, permitindo a seleção e remoção definitiva através de uma caixa de diálogo.
+ * </p>
+ */
 public class ApagarContas extends JPanel {
 
     private Utilizador userLogado;
     private AdminController adminController =  new AdminController();
+
+
+
+    /**
+     * Construtor da classe que inicializa a interface gráfica e carrega a listagem de contas a apagar
+     * <p>
+     * O construtor configura o aspeto visual do painel e invoca o método ({@code adminController.verContasPorApagar})
+     * para obter as contas da base de dados. Se a lista for válida, preenche uma {@link JTable} com o ID e o
+     * nome de utilizador de cada conta. Adicionalmente, configura o ouvinte de eventos ({@link ActionListener})
+     * para o botão de eliminação, que solicita ao administrador o ID do utilizador a remover, dessa forma validando a
+     * operação na base de dados através do método ({@code adminController.apagarContas}).
+     * </p>
+     * @param u O objeto {@link Utilizador} que representa o administrador com sessão iniciada, utilizado para
+     * validar as permissões de acesso às operações de listagem e eliminação de contas.
+     * @throws SQLException Se existir algum erro na comunicação com a base de dados ao tentar ler a lista de
+     * contas por apagar através do método ({@code adminController.verContasPorApagar}).
+     */
 
     public ApagarContas(Utilizador u) throws SQLException {
         this.userLogado = u;
